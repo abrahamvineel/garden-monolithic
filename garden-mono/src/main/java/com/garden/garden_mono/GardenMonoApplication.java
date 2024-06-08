6f4505bc53bda9a9a4426ccca229ebe3980232c9
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-06-08 14:18:15 -0400

    garden monolithic app init

---
+++ garden-mono/src/main/java/com/garden/garden_mono/GardenMonoApplication.java
@@ -0,0 +1,13 @@
+package com.garden.garden_mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenMonoApplication.class, args);
+	}
+
+}
---
+++ garden-mono/src/test/java/com/garden/garden_mono/GardenMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden.garden_mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
