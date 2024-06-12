cdaa2d1d4618d8f6c4e92c0bf77e8550d6754114
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-06-11 20:57:59 -0400

    invdentory service

--- garden-mono/src/main/java/com/garden/garden_mono/controller/InventoryController.java
+++ garden-mono/src/main/java/com/garden/garden_mono/controller/InventoryController.java
@@ -1,11 +1,20 @@
 package com.garden.garden_mono.controller;
 
+import com.garden.garden_mono.service.InventoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 public class InventoryController {
+
+    private InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
 }
---
+++ garden-mono/src/main/java/com/garden/garden_mono/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.garden.garden_mono.repository;
+
+import com.garden.garden_mono.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
